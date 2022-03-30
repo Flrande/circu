4e48af5483d8fb92511835d9e7d09ee82b0c04b2
Flrande <1978616327@qq.com>
2022-03-30 16:25:46 +0800

    🔵other: update main.tsx for React18

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import * as ReactDOMClient from "react-dom/client"
 import App from "./App"
 
 import "./normalize.css"
 
-ReactDOM.render(
+const container = document.getElementById("root")
+if (!container) throw "Can't find root dom."
+
+const root = ReactDOMClient.createRoot(container)
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
