89ba8a25d9ec42ad6aeeb0d63ef926eae4e98f49
Flrande <1978616327@qq.com>
2022-04-03 15:27:44 +0800

    ✨feature: update CodeArea about limiting selection

--- src/slate/components/Nodes/Block/BlockCode/BlockCode.tsx
+++ src/slate/components/Nodes/Block/BlockCode/BlockCode.tsx
@@ -13,57 +13,6 @@ const BlockCode: React.FC<CustomRenderElementProps<BlockCodeType>> = ({ attribut
 
   const langOptions: KeysUnion<CodeAreaLangMap>[] = ["PlainText", "Javascript"]
 
-  // TODO-BUG:
-  //
-  // -- 2022-4-1 --
-  // Edge, Chrome 上出现滚动条后选区限制失效, Firefox 完全失效
-  // 前者考虑为谷歌内核的问题, 后者考虑为浏览器之间对 mousemove event 的实现不一致
-  // 具体见下
-  // https://github.com/w3c/uievents/issues/278
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=1219471
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=346473
-  // 因而在找到绕过方式前暂时注释掉该段代码
-  //
-
-  // // 用于限制用户的选区
-  // // 选择起始点在代码块内时无法选择代码块外的内容
-  // const [containerDom, setContainerDom] = useState<HTMLElement>()
-
-  // useEffect(() => {
-  //   if (containerDom) {
-  //     const onDocumentMouseMove = (event: Event) => {
-  //       event.preventDefault()
-  //     }
-  //     const onDocumentMouseUp = () => {
-  //       containerDom.removeEventListener("mouseenter", onDomMouseEnter)
-  //       document.removeEventListener("mousemove", onDocumentMouseMove)
-  //     }
-  //     const onDomMouseEnter = () => {
-  //       document.removeEventListener("mousemove", onDocumentMouseMove)
-  //     }
-  //     const onDomMouseLeave = () => {
-  //       const sel = document.getSelection()
-  //       if (sel && containerDom.contains(sel.anchorNode)) {
-  //         document.addEventListener("mousemove", onDocumentMouseMove)
-  //         containerDom.addEventListener("mouseenter", onDomMouseEnter)
-  //       } else {
-  //         document.removeEventListener("mousemove", onDocumentMouseMove)
-  //       }
-  //     }
-
-  //     containerDom.addEventListener("mouseleave", onDomMouseLeave)
-  //     document.addEventListener("mouseup", onDocumentMouseUp)
-
-  //     return () => {
-  //       containerDom.removeEventListener("mouseleave", onDomMouseLeave)
-  //     }
-  //   }
-  // }, [containerDom, document.body.clientWidth])
-
-  // useEffect(() => {
-  //   setContainerDom(ReactEditor.toDOMNode(editor, element))
-  // }, [editor, element])
-
   return (
     <div
       {...attributes}
--- src/slate/components/Nodes/Block/BlockCode/BlockCode_CodeArea.tsx
+++ src/slate/components/Nodes/Block/BlockCode/BlockCode_CodeArea.tsx
@@ -1,13 +1,68 @@
+import { useEffect, useState } from "react"
+import { ReactEditor, useSlate } from "slate-react"
 import type { CustomRenderElementProps } from "../../../../types/utils"
 import type { BlockCode_CodeAreaType } from "./types"
 
-const BlockCode_CodeArea: React.FC<CustomRenderElementProps<BlockCode_CodeAreaType>> = ({ attributes, children }) => {
+const BlockCode_CodeArea: React.FC<CustomRenderElementProps<BlockCode_CodeAreaType>> = ({
+  attributes,
+  children,
+  element,
+}) => {
+  const editor = useSlate()
+  const [containerDom, setContainerDom] = useState<HTMLElement>()
+  const [ifEditable, setIfEditable] = useState<false | undefined>(false)
+
+  // 用于将用户选区限制在代码块内
+  useEffect(() => {
+    if (containerDom) {
+      const controller = new AbortController()
+      containerDom.addEventListener(
+        "mousedown",
+        () => {
+          setIfEditable(undefined)
+
+          containerDom.addEventListener(
+            "mouseleave",
+            () => {
+              setIfEditable(false)
+            },
+            {
+              signal: controller.signal,
+            }
+          )
+        },
+        {
+          signal: controller.signal,
+        }
+      )
+
+      document.addEventListener(
+        "mouseup",
+        () => {
+          setIfEditable(undefined)
+        },
+        {
+          signal: controller.signal,
+        }
+      )
+
+      return () => {
+        controller.abort()
+      }
+    }
+  }, [containerDom])
+
+  useEffect(() => {
+    setContainerDom(ReactEditor.toDOMNode(editor, element))
+  }, [editor, element])
+
   return (
     <div
       {...attributes}
       style={{
         width: "100%",
       }}
+      contentEditable={ifEditable}
     >
       {children}
     </div>
