8f20b857a1a51960d0ebd81cd2f93e9b56cb5497
Flrande <1978616327@qq.com>
2022-07-15 09:24:36 +0800

    ✨feature: improve toggleOrderedList

--- packages/circu-editor/src/slate/components/Nodes/Block/List/listHelper.ts
+++ packages/circu-editor/src/slate/components/Nodes/Block/List/listHelper.ts
@@ -1,6 +1,7 @@
-import { Editor, NodeEntry, Transforms } from "slate"
+import { Editor, NodeEntry, Path, Transforms } from "slate"
 import { CUSTOM_ELEMENT_PROPS_EXCEPT_CHILDREN } from "../../../../types/constant"
 import type { BlockElementExceptTextLine } from "../../../../types/interface"
+import { SlateElement } from "../../../../types/slate"
 import type { IQuote } from "../Quote/types"
 import { getSelectedBlocks } from "../utils/getSelectedBlocks"
 import type { IOrderedList, IUnorderedList } from "./types"
@@ -54,7 +55,6 @@ const unToggleList = (editor: Editor): void => {
   })
 }
 
-//TODO: 优化交互
 export const toggleOrderedList = (editor: Editor): void => {
   Editor.withoutNormalizing(editor, () => {
     const { selection } = editor
@@ -73,7 +73,40 @@ export const toggleOrderedList = (editor: Editor): void => {
         return
       }
 
-      for (const [index, [, path]] of selectedBlocks.entries()) {
+      for (const [, path] of selectedBlocks) {
+        // 决定当前块级节点转化为有序列表后是否为列表头
+        let currentBlockFlag = true
+
+        // 如果当前块级节点相邻前有同深度有序列表, 其转化为有序列表后类型应为自增
+        try {
+          const previousBlock = Editor.node(editor, Path.previous(path))
+          if (SlateElement.isElement(previousBlock[0]) && previousBlock[0].type === "ordered-list") {
+            currentBlockFlag = false
+          }
+        } catch (error) {}
+
+        // 如果当前块级节点相邻后有同深度有序列表, 相邻后同深度有序列表类型应为自增
+        try {
+          const nextBlock = Editor.node(editor, Path.next(path))
+          if (SlateElement.isElement(nextBlock[0]) && nextBlock[0].type === "ordered-list") {
+            const [list, listPath] = nextBlock
+            if (list.indexState.type !== "selfIncrement") {
+              Transforms.setNodes(
+                editor,
+                {
+                  indexState: {
+                    type: "selfIncrement",
+                    index: list.indexState.index,
+                  },
+                },
+                {
+                  at: listPath,
+                }
+              )
+            }
+          }
+        } catch (error) {}
+
         Transforms.unsetNodes(editor, CUSTOM_ELEMENT_PROPS_EXCEPT_CHILDREN, {
           at: path,
         })
@@ -82,7 +115,7 @@ export const toggleOrderedList = (editor: Editor): void => {
           {
             type: "ordered-list",
             indexState: {
-              type: index === 0 ? "head" : "selfIncrement",
+              type: currentBlockFlag ? "head" : "selfIncrement",
               index: 1,
             },
           },
