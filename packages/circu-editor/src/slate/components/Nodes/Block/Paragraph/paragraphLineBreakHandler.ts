6f601fd52d8d2989938991957bd8d6f9a024e620
Flrande <1978616327@qq.com>
2022-07-16 14:47:18 +0800

    ✨feature: update paragraphLineBreakHandler

--- packages/circu-editor/src/slate/components/Nodes/Block/Paragraph/paragraphLineBreakHandler.ts
+++ packages/circu-editor/src/slate/components/Nodes/Block/Paragraph/paragraphLineBreakHandler.ts
@@ -25,17 +25,34 @@ export const paragraphLineBreakHandler = (
   const [currentBlock, currentBlockPath] = currentEntry
 
   if (SlateRange.isCollapsed(selection) && currentBlock.type === "paragraph") {
+    // 若含子节点
     if (currentBlock.children.length === 2) {
-      // 若含子节点, 将换行拆出的部分移到子节点块中
-      Transforms.splitNodes(editor, {
-        at: selection.anchor,
-        match: (n) => SlateElement.isElement(n) && arrayIncludes(BLOCK_ELEMENTS_EXCEPT_TEXT_LINE, n.type),
-        always: true,
-      })
-      increaseIndent(editor, Editor.range(editor, Path.next(currentBlockPath)))
-      decreaseIndent(editor, Editor.range(editor, currentBlockPath.concat([1, 0, 1])))
+      // 若含子节点且折叠, 换行产生兄弟节点
+      if (currentBlock.isFolded) {
+        Transforms.splitNodes(editor, {
+          at: selection.anchor,
+          match: (n) => SlateElement.isElement(n) && arrayIncludes(BLOCK_ELEMENTS_EXCEPT_TEXT_LINE, n.type),
+          always: true,
+        })
 
-      Transforms.select(editor, Editor.start(editor, currentBlockPath.concat([1])))
+        // 切割后当前段落的子节点块会成为新段落的子节点块, 因此要移回去
+        Transforms.moveNodes(editor, {
+          at: Path.next(currentBlockPath).concat([1]),
+          to: currentBlockPath.concat([1]),
+        })
+
+        // 若含子节点且非折叠, 将换行拆出的部分移到子节点块中
+      } else {
+        Transforms.splitNodes(editor, {
+          at: selection.anchor,
+          match: (n) => SlateElement.isElement(n) && arrayIncludes(BLOCK_ELEMENTS_EXCEPT_TEXT_LINE, n.type),
+          always: true,
+        })
+        increaseIndent(editor, Editor.range(editor, Path.next(currentBlockPath)))
+        decreaseIndent(editor, Editor.range(editor, currentBlockPath.concat([1, 0, 1])))
+
+        Transforms.select(editor, Editor.start(editor, currentBlockPath.concat([1])))
+      }
     } else {
       Transforms.splitNodes(editor, {
         at: selection.anchor,
