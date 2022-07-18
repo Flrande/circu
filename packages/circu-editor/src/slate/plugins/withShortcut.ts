07553dc8bdb7593764a6bb5a32bf82947fa9d4f8
Flrande <1978616327@qq.com>
2022-07-18 18:37:13 +0800

    ✨feature: add markdown shortcuts for inline style

--- packages/circu-editor/src/slate/plugins/withShortcut.ts
+++ packages/circu-editor/src/slate/plugins/withShortcut.ts
@@ -1,9 +1,11 @@
 import { Editor, Transforms } from "slate"
 import { toggleHead } from "../components/Nodes/Block/Head/headHelper"
 import { toggleOrderedList, toggleUnorderedList } from "../components/Nodes/Block/List/listHelper"
-import { SlateRange, SlateText } from "../types/slate"
+import { toggleMark } from "../components/Nodes/Text/textHelper"
+import { SlateElement, SlateRange, SlateText } from "../types/slate"
 
-const shortcuts = ["#", "##", "###", "####", "#####", "######", "1.", "-", "*"]
+const singleBlockShortcuts = ["#", "##", "###", "####", "#####", "######", "1.", "-", "*"]
+const inlineShortcuts = ["**", "*", "~", "~~"]
 
 export const withShortcut = (editor: Editor) => {
   const { insertText } = editor
@@ -11,23 +13,23 @@ export const withShortcut = (editor: Editor) => {
   editor.insertText = (text) => {
     const { selection } = editor
     if (text === " " && selection && SlateRange.isCollapsed(selection)) {
-      const textNodes = Array.from(
+      const paragraphs = Array.from(
         Editor.nodes(editor, {
           at: selection.anchor,
-          match: (n) => SlateText.isText(n),
+          match: (n) => SlateElement.isElement(n) && n.type === "paragraph",
         })
       )
 
-      if (textNodes.length > 0) {
-        const [, textNodePath] = textNodes[0]
+      if (paragraphs.length > 0) {
+        const [, paragraphPath] = paragraphs[0]
 
-        const startPoint = Editor.start(editor, textNodePath)
+        const startPoint = Editor.start(editor, paragraphPath)
         const beforeString = Editor.string(editor, {
           anchor: startPoint,
           focus: selection.anchor,
         })
 
-        if (shortcuts.includes(beforeString)) {
+        if (singleBlockShortcuts.includes(beforeString)) {
           Transforms.delete(editor, {
             at: {
               anchor: startPoint,
@@ -66,6 +68,91 @@ export const withShortcut = (editor: Editor) => {
           }
 
           return
+        }
+      }
+
+      const texts = Array.from(
+        Editor.nodes(editor, {
+          at: selection.anchor,
+          match: (n) => SlateText.isText(n),
+        })
+      )
+
+      if (texts.length > 0) {
+        const [, textPath] = texts[0]
+
+        const startPoint = Editor.start(editor, textPath)
+        const beforeTextString = Editor.string(editor, {
+          anchor: startPoint,
+          focus: selection.anchor,
+        })
+
+        const reverseString = [...beforeTextString].reverse().join("")
+        for (const inlineShortcut of inlineShortcuts) {
+          if (reverseString.startsWith(inlineShortcut)) {
+            const found = reverseString
+              .slice(inlineShortcut.length)
+              .match(inlineShortcut === "**" ? /\*\*/ : inlineShortcut === "*" ? /\*/ : inlineShortcut)
+            if (found && found.index) {
+              // beforeTextString = aaa**bbb**
+              // 计算 bbb 的位置
+              // found.index = 3
+              // 10 - 2 - 3
+              const matchStartIndex = beforeTextString.length - inlineShortcut.length - found.index
+              // 删去 **bbb**
+              Transforms.delete(editor, {
+                at: {
+                  anchor: {
+                    path: textPath,
+                    offset: matchStartIndex - inlineShortcut.length,
+                  },
+                  focus: selection.anchor,
+                },
+              })
+              // 插入 bbb
+              Transforms.insertText(editor, beforeTextString.slice(matchStartIndex, -inlineShortcut.length), {
+                at: {
+                  path: textPath,
+                  offset: matchStartIndex - inlineShortcut.length,
+                },
+              })
+
+              Transforms.select(editor, {
+                anchor: {
+                  path: textPath,
+                  offset: matchStartIndex - inlineShortcut.length,
+                },
+                focus: {
+                  path: textPath,
+                  offset:
+                    matchStartIndex -
+                    inlineShortcut.length +
+                    beforeTextString.slice(matchStartIndex, -inlineShortcut.length).length,
+                },
+              })
+              switch (inlineShortcut) {
+                case "**":
+                  toggleMark(editor, "bold", true)
+                  break
+                case "*":
+                  toggleMark(editor, "italic", true)
+                  break
+                case "~":
+                  toggleMark(editor, "underline", true)
+                  break
+                case "~~":
+                  toggleMark(editor, "strike", true)
+                  break
+                default:
+                  break
+              }
+              if (editor.selection) {
+                Transforms.select(editor, Editor.end(editor, editor.selection))
+              }
+
+              return
+            }
+          }
         }
       }
     }
