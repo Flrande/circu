507510659f6c4a05baa8555169beb513d2536b8f
Flrande <1978616327@qq.com>
2022-08-21 11:32:19 +0800

    ✨(circu-wiki): add resize col in sidebar

--- packages/circu-wiki/src/page/home/components/SideBar/SideBar.tsx
+++ packages/circu-wiki/src/page/home/components/SideBar/SideBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import ScrollBar from "smooth-scrollbar"
 import { useFoldersId } from "../../../../server/folder"
 import Folder from "./Folder"
@@ -6,16 +6,73 @@ import Folder from "./Folder"
 const SideBar: React.FC = () => {
   const { foldersId, errorGetFolders } = useFoldersId()
 
+  const [isResizeColDrag, setIsResizeColDrag] = useState(false)
+  const [sideBarWidth, setSideBarWidth] = useState(
+    window.localStorage.getItem("sidebar_width") ? parseInt(window.localStorage.getItem("sidebar_width")!) : 288
+  )
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    if (isResizeColDrag) {
+      document.addEventListener(
+        "mousemove",
+        (event) => {
+          if (isResizeColDrag && event.screenX >= 220 && event.screenX <= 480) {
+            setSideBarWidth(event.screenX)
+          }
+        },
+        {
+          signal: controller.signal,
+        }
+      )
+      document.addEventListener(
+        "mouseup",
+        () => {
+          setIsResizeColDrag(false)
+        },
+        {
+          signal: controller.signal,
+        }
+      )
+    }
+
+    return () => {
+      controller.abort()
+    }
+  }, [isResizeColDrag])
+
+  useEffect(() => {
+    // 将用户的设置持久化到 localStorage
+    window.localStorage.setItem("sidebar_width", sideBarWidth.toString())
+  }, [sideBarWidth])
+
   useEffect(() => {
     ScrollBar.init(document.getElementById("side-bar-root")!)
   }, [])
 
   return (
-    <div id={"side-bar-root"} className={"basis-72 shrink-0 bg-[#202020]"}>
-      <div className={"bg-transparent h-12 mb-11"}></div>
-      <div className={"bg-transparent p-4"}>
-        {foldersId ? foldersId.map((id) => <Folder key={id} folderId={id}></Folder>) : <div></div>}
+    <div
+      className={"shrink-0 bg-[#202020] h-full relative z-10"}
+      style={{
+        flexBasis: `${sideBarWidth}px`,
+      }}
+    >
+      <div id={"side-bar-root"} className={"h-full"}>
+        <div className={"bg-transparent h-12 mb-11"}></div>
+        <div className={"bg-transparent p-4"}>
+          {foldersId ? foldersId.map((id) => <Folder key={id} folderId={id}></Folder>) : <div></div>}
+        </div>
       </div>
+      <div
+        onMouseDown={() => {
+          setIsResizeColDrag(true)
+        }}
+        className={"absolute -right-[2px] top-0 h-full w-[2px] bg-transparent hover:bg-[#3d3d3d] cursor-col-resize"}
+        style={{
+          backgroundColor: isResizeColDrag ? "#3d3d3d" : undefined,
+        }}
+      ></div>
     </div>
   )
 }
