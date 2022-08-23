0e3027abaf4d5410bf8c5ef75787ab1382f88ab9
Flrande <1978616327@qq.com>
2022-08-23 16:58:22 +0800

    🔵(circu-editor): update exposed interface

--- packages/circu-editor/src/index.ts
+++ packages/circu-editor/src/index.ts
@@ -2,4 +2,8 @@ export * from "./slate/hooks/useCircuEditor"
 export * from "./CircuEditor"
 
 export * from "./slate/types/interface"
-export * from "./slate/types/slate"
+
+import { Editor as SlateEditor, Element as SlateElement } from "slate"
+import { ReactEditor as SlateReactEditor } from "slate-react"
+
+export { SlateEditor, SlateElement, SlateReactEditor }
