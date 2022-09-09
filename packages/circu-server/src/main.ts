ad0cd3384abec2dacddb161d6394220d89169367
Flrande <1978616327@qq.com>
2022-09-09 16:14:04 +0800

    🔵(circu-server): add package for session

--- packages/circu-server/src/main.ts
+++ packages/circu-server/src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import * as session from "express-session"
+import { ConfigService } from "@nestjs/config"
+import Redis from "ioredis"
+
+const RedisStore = require("connect-redis")(session)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const configService = app.get(ConfigService)
+  const redisClient = new Redis()
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +21,16 @@ async function bootstrap() {
       dismissDefaultMessages: true,
     })
   )
+
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      secret: configService.getOrThrow("SESSION_SECRET"),
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+
   await app.listen(6000)
 }
 bootstrap()
