40890bfcdebcd2115283cb20fa6906d427e2942e
Flrande <1978616327@qq.com>
2022-09-11 14:26:29 +0800

    🔵(circu-server): revise some notes

--- packages/circu-server/src/exception/common.exception.ts
+++ packages/circu-server/src/exception/common.exception.ts
@@ -8,7 +8,7 @@ export type ICommonException = {
   isFiltered: boolean
 }
 
-// isFiltered 字段用于标识该异常是否暴露给前端
+// isFiltered 字段用于标识该异常是否暴露给前端, 若为真, 不暴露
 export class CommonException extends HttpException {
   constructor(data: Partial<ICommonException>) {
     const baseData: ICommonException = {
