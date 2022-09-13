7ec06b4268c1d467974b984d55c652191c8f1e88
Flrande <1978616327@qq.com>
2022-09-13 19:40:53 +0800

    ✨(circu-server): create user auth guard

---
+++ packages/circu-server/src/guards/user-auth.guard.ts
@@ -0,0 +1,35 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
+import { Request } from "express"
+import { PrismaService } from "src/database/prisma.service"
+
+/**
+ * 一个用于判断请求是否在内存数据库中有对应会话的守卫组件
+ */
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>()
+    const userid = request.session.userid
+
+    if (!userid) {
+      return false
+    }
+
+    const result = await this.prismaService.user.findUnique({
+      where: {
+        id: userid,
+      },
+      select: {
+        id: true,
+      },
+    })
+
+    if (!result) {
+      return false
+    }
+
+    return true
+  }
+}
