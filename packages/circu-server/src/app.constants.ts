c1360c7ac0363379c0a290030f0c26108cc7b75b
Flrande <1978616327@qq.com>
2022-09-14 09:15:22 +0800

    🔵(circu-server): replace enum with const

--- packages/circu-server/src/app.constants.ts
+++ packages/circu-server/src/app.constants.ts
@@ -1,4 +1,4 @@
-export enum GlobalExceptionCode {
+export const GlobalExceptionCode = {
   // 每个异常都有对应的错误码, 但除了是前端需要处理的异常, 返回时统一使用一个错误码
-  COMMON_EXCEPTION_CODE = 999,
-}
+  COMMON_EXCEPTION_CODE: 999,
+} as const
--- packages/circu-server/src/user/user.constants.ts
+++ packages/circu-server/src/user/user.constants.ts
@@ -1,5 +1,5 @@
 // user 相关错误范围 1000-1199
-export enum UserExceptionCode {
-  USER_NOT_FOUND = 1000,
-  USER_IS_EXISTING = 1001,
-}
+export const UserExceptionCode = {
+  USER_NOT_FOUND: 1000,
+  USER_IS_EXISTING: 1001,
+} as const
