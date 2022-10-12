8b84f60cce3cf0ab513689519a489a75ebd59702
Flrande <1978616327@qq.com>
2022-10-12 10:59:35 +0800

    🔵(circu-server): change relation name about fast access

---
+++ packages/circu-server/prisma/migrations/20221012025611_change_relation_name_about_fast_access/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_folders_to_users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_folders_to_users" DROP CONSTRAINT "_folders_to_users_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_folders_to_users" DROP CONSTRAINT "_folders_to_users_B_fkey";
+
+-- DropTable
+DROP TABLE "_folders_to_users";
+
+-- CreateTable
+CREATE TABLE "_fast_access_folders_to_users" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_fast_access_folders_to_users_AB_unique" ON "_fast_access_folders_to_users"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_fast_access_folders_to_users_B_index" ON "_fast_access_folders_to_users"("B");
+
+-- AddForeignKey
+ALTER TABLE "_fast_access_folders_to_users" ADD CONSTRAINT "_fast_access_folders_to_users_A_fkey" FOREIGN KEY ("A") REFERENCES "folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_fast_access_folders_to_users" ADD CONSTRAINT "_fast_access_folders_to_users_B_fkey" FOREIGN KEY ("B") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
