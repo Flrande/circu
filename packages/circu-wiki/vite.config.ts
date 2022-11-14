7b61aa4a19224af6e4f625df178192aede1b0c02
Flrande <1978616327@qq.com>
2022-11-14 11:12:51 +0800

    🔵(circu-wiki): add vite-plugin-importus for reducing build volume

--- packages/circu-wiki/vite.config.ts
+++ packages/circu-wiki/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import vitePluginForArco from "@arco-plugins/vite-react"
+import vitePluginImportus from "vite-plugin-importus"
 
 export default defineConfig({
-  plugins: [react(), vitePluginForArco({ style: "css" })],
+  plugins: [
+    react(),
+    vitePluginForArco({ style: "css" }),
+    vitePluginImportus([
+      {
+        libraryName: "@icon-park/react",
+        customName: (name: string) => `@icon-park/react/es/icons/${name}`,
+        camel2DashComponentName: false,
+      },
+    ]),
+  ],
   server: {
     port: 5000,
   },
