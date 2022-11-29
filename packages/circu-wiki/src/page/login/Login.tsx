3c5d47704a4f8fd4526c46001a0679abb7775e7c
Flrande <1978616327@qq.com>
2022-11-29 16:52:59 +0800

    🚧(circu-wiki): update login page

--- packages/circu-wiki/src/page/login/Login.tsx
+++ packages/circu-wiki/src/page/login/Login.tsx
@@ -1,9 +1,10 @@
 import loginPageBgUrl from "./global-bg.jpg"
 import logoUrl from "../../styles/logo.png"
-import { GithubOne } from "@icon-park/react"
+import { CheckSmall, GithubOne } from "@icon-park/react"
 import { useMutation } from "@tanstack/react-query"
-import { Login, login, LoginData, LoginError, LoginProps } from "../../server/login"
-import { proxy } from "valtio"
+import { login, LoginData, LoginError, LoginProps } from "../../server/login"
+import { proxy, useSnapshot } from "valtio"
+import { Message, Spin } from "@arco-design/web-react"
 
 type LoginStateStore = {
   loginPayload: LoginProps
@@ -20,8 +21,26 @@ const loginStateStore = proxy<LoginStateStore>({
 })
 
 const Login: React.FC = () => {
+  const loginStateSnap = useSnapshot(loginStateStore)
+
   const loginMutation = useMutation<LoginData, LoginError, LoginProps>({
     mutationFn: ({ username, password, options }) => login(username, password, options),
+    onError: () => {
+      //TODO: 错误码处理
+      Message.error({
+        content: "登录失败, 请重试.",
+        className: "rounded-md",
+      })
+    },
+    onSuccess: () => {
+      Message.info({
+        icon: <Spin></Spin>,
+        content: "登录成功, 跳转中, 请稍等.",
+        className: "rounded-md",
+      })
+
+      //TODO: 主页跳转
+    },
   })
 
   return (
@@ -88,7 +107,17 @@ const Login: React.FC = () => {
             </div>
             <div className={"grid grid-cols-[340px_1fr] px-2 py-3"}>
               <div className={"grid grid-cols-[32px_1fr] items-center"}>
-                <div className={"w-5 h-5 rounded-md bg-[#323645] border-[#467cab] cursor-pointer"}></div>
+                <div
+                  onClick={() => {
+                    loginStateStore.loginPayload.options.ifCarrySession =
+                      !loginStateStore.loginPayload.options.ifCarrySession
+                  }}
+                  className={"w-5 h-5 rounded-md bg-[#343435] hover:bg-[#3e3e3f] border-[#467cab] cursor-pointer"}
+                >
+                  {loginStateSnap.loginPayload.options.ifCarrySession && (
+                    <CheckSmall theme="filled" size="20" fill="#ffffff" strokeLinejoin="bevel" strokeLinecap="square" />
+                  )}
+                </div>
                 <div className={"text-xl text-white"}>
                   <span>7天内免登录</span>
                 </div>
@@ -99,6 +128,9 @@ const Login: React.FC = () => {
             </div>
             <div className={"px-2 py-3"}>
               <div
+                onClick={() => {
+                  loginMutation.mutate(loginStateStore.loginPayload)
+                }}
                 className={
                   "w-full h-full rounded-md bg-[#416ef6] hover:bg-[#467cab] grid justify-center items-center text-xl text-white cursor-pointer"
                 }
