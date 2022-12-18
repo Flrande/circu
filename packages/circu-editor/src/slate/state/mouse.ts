caca026be4a761d1eaf3250653b87c8af8f29573
Flrande <1978616327@qq.com>
2022-12-18 18:31:25 +0800

    🐞(circu-editor): fix interactive bug related to dragging button

--- packages/circu-editor/src/slate/state/mouse.ts
+++ packages/circu-editor/src/slate/state/mouse.ts
@@ -13,14 +13,18 @@ export const useMouseXBlockDetect: (element: SlateElement) => {
   const editor = useSlateStatic()
 
   // 要在回调里取才能拿到最新的 path, 否则有可能会过期
-  const onMouseEnterForDrag = () => {
+  const onMouseEnterForDrag: React.MouseEventHandler = () => {
     const path = ReactEditor.findPath(editor, element)
     mouseXStateStore.xBlockPath = path
   }
-  const onMouseLeaveForDrag = () => {
-    const path = ReactEditor.findPath(editor, element)
-    if (path.length >= 3) {
-      mouseXStateStore.xBlockPath = path.slice(0, path.length - 2)
+  const onMouseLeaveForDrag: React.MouseEventHandler = (event) => {
+    // 如果移出后的节点是 block 节点才调整 xBlockPath, 因为移出去后有可能是 BlockChildren,
+    // 这时候调整 xBlockPath 用户无法在当前行选中拖动按钮
+    if (event.relatedTarget && (event.relatedTarget as HTMLElement).dataset.circuNode === "block") {
+      const path = ReactEditor.findPath(editor, element)
+      if (path.length >= 3) {
+        mouseXStateStore.xBlockPath = path.slice(0, path.length - 2)
+      }
     }
   }
 
