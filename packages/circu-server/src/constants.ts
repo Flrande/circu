351125d4547b8de355e175665096bb463529baed
Flrande <1978616327@qq.com>
2022-12-19 22:00:29 +0800

    🔵(circu-server): update corsOrigins

--- packages/circu-server/src/constants.ts
+++ packages/circu-server/src/constants.ts
@@ -1,4 +1,4 @@
-export const corsOrigins: string[] = ["http://localhost:5000"]
+export const corsOrigins: string[] = ["https://localhost:5000"]
 if (process.env.CLIENT_URL) {
   corsOrigins.push(process.env.CLIENT_URL)
 }
