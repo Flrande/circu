f5df6a4792aa8b250d308ceafa515c4ff6112f6e
Flrande <1978616327@qq.com>
2022-12-23 23:51:44 +0800

    🐞(circu-editor): fix error about drag button

--- packages/circu-editor/src/slate/state/mouse.ts
+++ packages/circu-editor/src/slate/state/mouse.ts
@@ -20,7 +20,11 @@ export const useMouseXBlockDetect: (element: SlateElement) => {
   const onMouseLeaveForDrag: React.MouseEventHandler = (event) => {
     // 如果移出后的节点是 block 节点才调整 xBlockPath, 因为移出去后有可能是 BlockChildren,
     // 这时候调整 xBlockPath 用户无法在当前行选中拖动按钮
-    if (event.relatedTarget && (event.relatedTarget as HTMLElement).dataset.circuNode === "block") {
+    if (
+      event.relatedTarget &&
+      (event.relatedTarget as HTMLElement).dataset &&
+      (event.relatedTarget as HTMLElement).dataset.circuNode === "block"
+    ) {
       const path = ReactEditor.findPath(editor, element)
       if (path.length >= 3) {
         mouseXStateStore.xBlockPath = path.slice(0, path.length - 2)
