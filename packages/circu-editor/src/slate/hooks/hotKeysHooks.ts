3db49040916bbb4dc4ed6eedeeb8851188bfe601
Flrande <1978616327@qq.com>
2022-12-31 16:09:25 +0800

    🔵(circu-editor): optimize tab operation in code block

--- packages/circu-editor/src/slate/hooks/hotKeysHooks.ts
+++ packages/circu-editor/src/slate/hooks/hotKeysHooks.ts
@@ -1,27 +1,57 @@
-import type React from "react"
 import { useCallback } from "react"
-import { Editor, NodeEntry, Transforms } from "slate"
-import { useSlateStatic } from "slate-react"
+import { Editor, Transforms } from "slate"
+import { ReactEditor, useSlateStatic } from "slate-react"
 import { decreaseIndent, increaseIndent } from "../components/Nodes/Block/BlockWrapper/indentHelper"
 import { BLOCK_ELEMENTS_EXCEPT_TEXT_LINE } from "../types/constant"
 import { SlateElement } from "../types/slate"
 import { arrayIncludes } from "../utils/general"
 import { getSelectedBlocks } from "../components/Nodes/Block/utils/getSelectedBlocks"
-import type { BlockElementWithChildren } from "../types/interface"
 import { toggleTaskList } from "../components/Nodes/Block/TaskList/taskListHelper"
 
 export const useOnKeyDown = () => {
   const editor = useSlateStatic()
 
   return useCallback((event: React.KeyboardEvent<HTMLDivElement>) => {
+    // 在代码块中要通过 Ctrl + ArrowLeft 来减少缩进, 通过 Ctrl + ArrowRight 来增加缩进
+    if (event.ctrlKey && (event.key === "ArrowLeft" || event.key === "ArrowRight")) {
+      event.preventDefault()
+      if (editor.selection) {
+        const selectedBlocks = getSelectedBlocks(editor, {
+          range: editor.selection,
+        })
+        if (selectedBlocks.length !== 0 && selectedBlocks[0][0].type === "block-code") {
+          if (event.key === "ArrowLeft") {
+            decreaseIndent(editor)
+          } else if (event.key === "ArrowRight") {
+            increaseIndent(editor)
+          }
+
+          Transforms.select(editor, editor.selection)
+          ReactEditor.focus(editor)
+          return
+        }
+      }
+    }
+
     if (event.key === "Tab") {
       event.preventDefault()
-      if (event.shiftKey) {
-        decreaseIndent(editor)
-      } else {
-        increaseIndent(editor)
+      if (editor.selection) {
+        const selectedBlocks = getSelectedBlocks(editor, {
+          range: editor.selection,
+        })
+        if (selectedBlocks.length !== 0 && selectedBlocks[0][0].type === "block-code") {
+          // 在代码块中按下 Tab 时, 不会调整块级节点的缩进, 而是插入4个空格
+          Transforms.insertText(editor, "    ")
+          return
+        }
+
+        if (event.shiftKey) {
+          decreaseIndent(editor)
+        } else {
+          increaseIndent(editor)
+        }
+        return
       }
-      return
     }
 
     // --------------------------------------------------
